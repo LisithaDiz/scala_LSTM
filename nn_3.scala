065aaecc6ec75323071305a747ee41780d2c6e3d
Lisitha Dissanayaka <114801465+LisithaDiz@users.noreply.github.com>
2024-06-28 20:53:17 +0530

    Update nn_3.scala

--- nn_3.scala
+++ nn_3.scala
@@ -10,13 +10,19 @@ object check {
   )
 
   val B = Array(
+    Array(0.2, 0.8, -0.5),
+    Array(0.5, -0.91, 0.26),
+    Array(-0.26, -0.27, 0.17)
+  )
+  val C = Array(
     Array(0.2, 0.8, -0.5, 1.0),
     Array(0.5, -0.91, 0.26, -0.5),
     Array(-0.26, -0.27, 0.17, 0.87)
   )
 
+
   def main(args: Array[String]): Unit = {
-    val result = multiply(B, A)
+    val result = multiply(B, B)
     println("Result of multiplication:")
     result.foreach(row => println(row.mkString(" ")))
   }
