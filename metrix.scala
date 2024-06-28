deb344d03e561651dd7769e4c0c3d42cb995f80e
Lisitha Dissanayaka <114801465+LisithaDiz@users.noreply.github.com>
2024-06-28 20:41:25 +0530

    Update metrix.scala

--- metrix.scala
+++ metrix.scala
@@ -4,8 +4,8 @@ package object DotOp {
 
   def dot(matrixA: Array[Double], matrixB: Array[Double]): Array[Double] = {
     if (matrixA.length == matrixB.length) {
-      var dot_Matrix: Array[Double] = new Array[Double](matrixA.length)
-      for (i <- 0 to matrixA.length) {
+      val dot_Matrix: Array[Double] = new Array[Double](matrixA.length)
+      for (i <- 0 until matrixA.length) {
         dot_Matrix(i) = matrixA(i) * matrixB(i)
       }
       dot_Matrix
@@ -16,7 +16,7 @@ package object DotOp {
 
   def dotNadd(matrix: Array[Double]): Double = {
     var sum = 0.0
-    for (i <- 0 to matrix.length) {
+    for (i <- 0 until matrix.length) {
       sum += matrix(i)
     }
     sum
@@ -29,8 +29,8 @@ package object matrixOp {
   def reshape(matrix: Array[Array[Double]]): Array[Array[Double]] = {
     val reshape_Matrix: Array[Array[Double]] = Array.ofDim[Double](matrix(0).length, matrix.length)
 
-    for (j <- 0 to matrix.length) {
-      for (i <- 0 to matrix(0).length) {
+    for (j <- 0 until matrix.length) {
+      for (i <- 0 until matrix(0).length) {
         reshape_Matrix(i)(j) = matrix(j)(i)
       }
     }
@@ -39,10 +39,10 @@ package object matrixOp {
 
   def multiply(matrixA: Array[Array[Double]], matrixB: Array[Array[Double]]): Array[Array[Double]] = {
     val reshape_matrixB = reshape(matrixB)
-    val new_Matrix: Array[Array[Double]] = Array.ofDim[Double](matrixA.length, reshape_matrixB(0).length)
+    val new_Matrix: Array[Array[Double]] = Array.ofDim[Double](matrixA.length, reshape_matrixB.length)
 
-    for (i <- 0 to matrixA.length) {
-      for (j <- 0 to reshape_matrixB.length) {
+    for (i <- 0 until matrixA.length) {
+      for (j <- 0 until reshape_matrixB.length) {
         new_Matrix(i)(j) = dotNadd(dot(matrixA(i), reshape_matrixB(j)))
       }
     }
